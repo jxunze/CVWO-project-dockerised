1396c253d4274cda327dc8e91da14e96457e1e10
jxunze <jinxunzejxz@gmail.com>
2023-01-23 00:37:02 +0800

    commit

---
+++ frontend/build/precache-manifest.57f3ed1806534735c8f2e892d9ce17f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "13ad563c149df3b0a8c6",
+    "url": "/static/css/main.205bd3f6.chunk.css"
+  },
+  {
+    "revision": "13ad563c149df3b0a8c6",
+    "url": "/static/js/main.13ad563c.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "39025c9571de66424111",
+    "url": "/static/css/2.20d3d585.chunk.css"
+  },
+  {
+    "revision": "39025c9571de66424111",
+    "url": "/static/js/2.39025c95.chunk.js"
+  },
+  {
+    "revision": "316baba7ca5f9e773de3",
+    "url": "/static/js/runtime~main.316baba7.js"
+  },
+  {
+    "revision": "e4bec84e2d9a90d466cbfc1edadbcbf9",
+    "url": "/index.html"
+  }
+];
